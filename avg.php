4749d5c132db97d93b968cbafb731651585363eb
Krystiankuznik1 <krystian15082003@gmail.com>
2025-03-08 18:25:52 +0100

    Manual merge. Wybór ostateczych wag

---
+++ avg.php
@@ -0,0 +1,16 @@
+<?php
+
+function srednia_zwykla (a.b)
+		return (a+b)/2;
+
+function srednia_wazona (a,b,a_heavier = true
+
+
+	if(a_heavier)
+	(
+		return 0.4*a + 0.6*b;
+	)
+	else	
+	(
+		return 0.3*a + 0.7*b;
+	)
